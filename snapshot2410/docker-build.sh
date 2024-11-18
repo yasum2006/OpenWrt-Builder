44ea4432631e703ed0b7ea72882fae46d7449679
qichiyu <h320117@gmail.com>
2024-11-18 00:21:15 -0500

    上传文件至 snapshot2410

---
+++ snapshot2410/docker-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run --rm -it \
+-v ./bin:/builder/bin \
+-v ./files:/builder/files \
+-v ./build.sh:/builder/build.sh \
+openwrt/imagebuilder:x86-64-24.10-SNAPSHOT /builder/build.sh
