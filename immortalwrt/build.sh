5a1910122c5c4aa829b39f9f69612c4a270f1acf
qichiyu <h320117@outlook.com>
2025-02-15 23:12:15 +0800

    更新为24.10版本

--- immortalwrt/build.sh
+++ immortalwrt/build.sh
@@ -2,8 +2,9 @@
 
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 
 
 make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="512"
