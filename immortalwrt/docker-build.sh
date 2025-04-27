23f5e334411bf1d70573c4e256c971bf8170b812
qichiyuhub <h320117@gmail.com>
2025-04-27 12:55:19 +0800

    更新为24.10.1

--- immortalwrt/docker-build.sh
+++ immortalwrt/docker-build.sh
@@ -4,4 +4,4 @@ docker run --rm -it \
 -v ./bin:/home/build/immortalwrt/bin \
 -v ./files:/home/build/immortalwrt/files \
 -v ./build.sh:/home/build/immortalwrt/build.sh \
-immortalwrt/imagebuilder:x86-64-openwrt-24.10.0 /home/build/immortalwrt/build.sh
+immortalwrt/imagebuilder:x86-64-openwrt-24.10.1 /home/build/immortalwrt/build.sh
--- stable/docker-build.sh
+++ stable/docker-build.sh
@@ -4,4 +4,4 @@ docker run --rm -it \
 -v ./bin:/builder/bin \
 -v ./files:/builder/files \
 -v ./build.sh:/builder/build.sh \
-openwrt/imagebuilder:x86-64-24.10.0 /builder/build.sh
+openwrt/imagebuilder:x86-64-24.10.1 /builder/build.sh
